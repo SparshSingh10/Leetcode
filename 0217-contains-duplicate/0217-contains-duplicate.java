c275092b3dd1cca60be177f2cb2ba5adeed4b8f0
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-01-02 22:17:39 +0530

    Time: 20 ms (53.46%), Space: 56.5 MB (65.48%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        int i;
+        Arrays.sort(nums);
+        for(i=0;i<nums.length-1;i++){
+                if(nums[i]==nums[i+1])
+                    return true;
+    }
+    return false;
+}
+}
