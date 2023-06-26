c4e99eb36e55e310fa26541b8386a6e74a651c49
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-06-26 22:33:02 +0530

    Time: 3 ms (95.41%), Space: 43.4 MB (74.52%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean canFinish(int V, int[][] prerequisites) {
+        int n = V;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            int a = prerequisite[0];
+            int b = prerequisite[1];
+            adj.get(a).add(b);
+        }
+
+        boolean[] visited = new boolean[V];
+        boolean[] currvisited = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (visited[i] == false) {
+                if (dfs(i, visited, currvisited, adj))
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean dfs(int node, boolean[] visited, boolean[] currvisited, List<List<Integer>> adj) {
+        visited[node] = true;
+        currvisited[node] = true;
+
+        for (int neighbour : adj.get(node)) {
+            if (visited[neighbour] == false) {
+                if (dfs(neighbour, visited, currvisited, adj))
+                    return true;
+            } else if (visited[neighbour] && currvisited[neighbour])
+                return true;
+        }
+
+        currvisited[node] = false;
+        return false;
+    }
+}
