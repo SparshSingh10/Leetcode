951ca464aaf63be55242378df0b88d78905a0337
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-06-26 09:02:38 +0530

    Time: 4 ms (51.48%), Space: 47.4 MB (93.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,36 +1,24 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1') {
-                    count++;
-                    dfs(i, j, n, m, grid);
-                }
+        int n=grid.length;
+        int m=grid[0].length;
+        int c=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1')
+                    c++;
+                dfs(i,j,n,m,grid);
             }
         }
-
-        return count;
+        return c;
     }
-
-    private void dfs(int i, int j, int n, int m, char[][] grid) {
-        if (!isValid(i, j, n, m, grid)) {
+    public void dfs(int i,int j,int n,int m,char[][]grid){
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') 
             return;
+            grid[i][j]='0';
+            dfs(i,j+1,n,m,grid);
+            dfs(i,j-1,n,m,grid);
+            dfs(i+1,j,n,m,grid);
+            dfs(i-1,j,n,m,grid);
         }
-
-        grid[i][j] = '0'; // Mark the current cell as visited
-
-        // Explore the neighboring cells in all four directions
-        dfs(i + 1, j, n, m, grid);
-        dfs(i - 1, j, n, m, grid);
-        dfs(i, j + 1, n, m, grid);
-        dfs(i, j - 1, n, m, grid);
     }
-
-    private boolean isValid(int i, int j, int n, int m, char[][] grid) {
-        return i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == '1';
-    }
-}
