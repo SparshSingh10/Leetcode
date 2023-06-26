1e90408ad00f9ac5ed35fb5399816ecb2b9825cf
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-06-26 08:55:08 +0530

    Time: 4 ms (51.48%), Space: 47.8 MB (66.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,53 +1,36 @@
-import java.util.ArrayList;
-
 class Solution {
-    private void dfs(int node, ArrayList<ArrayList<Integer>> adjLs, int[] vis) {
-        vis[node] = 1;
-        for (int i = 0; i < adjLs.get(node).size(); i++) {
-            int neighbor = adjLs.get(node).get(i);
-            if (vis[neighbor] == 0) {
-                dfs(neighbor, adjLs, vis);
-            }
-        }
-    }
-
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
 
-        ArrayList<ArrayList<Integer>> adjLs = new ArrayList<>();
-        for (int i = 0; i < m * n; i++) {
-            adjLs.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (grid[i][j] == '1') {
-                    int index = i * n + j;
-                    if (i > 0 && grid[i - 1][j] == '1') {
-                        int upIndex = (i - 1) * n + j;
-                        adjLs.get(index).add(upIndex);
-                        adjLs.get(upIndex).add(index);
-                    }
-                    if (j > 0 && grid[i][j - 1] == '1') {
-                        int leftIndex = i * n + (j - 1);
-                        adjLs.get(index).add(leftIndex);
-                        adjLs.get(leftIndex).add(index);
-                    }
+                    count++;
+                    dfs(i, j, n, m, grid);
                 }
             }
         }
 
-        int[] vis = new int[m * n];
-        int count = 0;
+        return count;
+    }
 
-        for (int i = 0; i < m * n; i++) {
-            if (vis[i] == 0 && grid[i / n][i % n] == '1') {
-                count++;
-                dfs(i, adjLs, vis);
-            }
+    private void dfs(int i, int j, int n, int m, char[][] grid) {
+        if (!isValid(i, j, n, m, grid)) {
+            return;
         }
 
-        return count;
+        grid[i][j] = '0'; // Mark the current cell as visited
+
+        // Explore the neighboring cells in all four directions
+        dfs(i + 1, j, n, m, grid);
+        dfs(i - 1, j, n, m, grid);
+        dfs(i, j + 1, n, m, grid);
+        dfs(i, j - 1, n, m, grid);
+    }
+
+    private boolean isValid(int i, int j, int n, int m, char[][] grid) {
+        return i >= 0 && i < n && j >= 0 && j < m && grid[i][j] == '1';
     }
 }
