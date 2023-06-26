76e574e5c41d747c2f53581ec8cfc29db8db8bd0
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-06-26 22:35:42 +0530

    Time: 4 ms (80.59%), Space: 43.5 MB (71.28%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canFinish(int V, int[][] prerequisites) {
-        int n = V;
+          int n = V;
         List<List<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             adj.add(new ArrayList<>());
@@ -11,33 +11,31 @@ class Solution {
             int b = prerequisite[1];
             adj.get(a).add(b);
         }
-
-        boolean[] visited = new boolean[V];
-        boolean[] currvisited = new boolean[V];
-
-        for (int i = 0; i < V; i++) {
-            if (visited[i] == false) {
-                if (dfs(i, visited, currvisited, adj))
-                    return false;
-            }
+        boolean[] visited=new boolean[V];
+        boolean[] currvisited=new boolean[V];
+        for(int i=0;i<V;i++){
+            if(visited[i]==false){
+                if(dfs(i,visited,currvisited,adj))
+                return false;
         }
-
+            
+        }
         return true;
+        
     }
-
-    public boolean dfs(int node, boolean[] visited, boolean[] currvisited, List<List<Integer>> adj) {
-        visited[node] = true;
-        currvisited[node] = true;
-
-        for (int neighbour : adj.get(node)) {
-            if (visited[neighbour] == false) {
-                if (dfs(neighbour, visited, currvisited, adj))
-                    return true;
-            } else if (visited[neighbour] && currvisited[neighbour])
+    public boolean dfs(int node,boolean[] visited,boolean[] currvisited,List<List<Integer>> adj){
+        visited[node]=true;
+        currvisited[node]=true;
+        for(int neighbour:adj.get(node)){
+            if(visited[neighbour]==false){
+                if(dfs(neighbour,visited,currvisited,adj))
                 return true;
+            }
+        else if(visited[neighbour] && currvisited[neighbour])
+            return true;
         }
-
-        currvisited[node] = false;
+        currvisited[node]=false;
         return false;
+        }
+    
     }
-}
