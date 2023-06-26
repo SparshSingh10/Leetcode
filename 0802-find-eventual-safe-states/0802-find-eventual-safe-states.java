a92ac39f1943617e738b669aa4f95f8401b3f0a0
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-06-26 15:49:03 +0530

    Time: 5 ms (99.05%), Space: 55.1 MB (34.28%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -0,0 +1,45 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> ans = new ArrayList<>();
+
+        int n = graph.length;
+        boolean[] visited = new boolean[n];
+        boolean[] dfsVisited = new boolean[n];
+        boolean[] presentCycle = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, dfsVisited, graph, presentCycle);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!presentCycle[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean dfs(int s, boolean[] visited, boolean[] dfsVisited, int[][] graph, boolean[] presentCycle) {
+        visited[s] = true;
+        dfsVisited[s] = true;
+
+        int[] data = graph[s];
+        for (int x : data) {
+            if (!visited[x]) {
+                if (dfs(x, visited, dfsVisited, graph, presentCycle)) {
+                    presentCycle[s] = true;
+                    return true;
+                }
+            } else if (visited[x] && dfsVisited[x]) {
+                presentCycle[s] = true;
+                return true;
+            }
+        }
+
+        dfsVisited[s] = false;
+        return false;
+    }
+}
