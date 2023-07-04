c7be5af82afc65140d12cc4dcbef9ce91d19299f
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-07-04 15:42:34 +0530

    Time: 1 ms (86.39%), Space: 43.5 MB (36.58%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,14 +1,14 @@
+import java.util.*;
+
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int numRooms = rooms.size();
-        boolean[] visited = new boolean[numRooms];
         
-        // Start the DFS from room 0
-        dfs(0, rooms, visited);
+        dfs(0, rooms);
         
         // Check if all rooms have been visited
-        for (boolean roomVisited : visited) {
-            if (roomVisited==false) {
+        for (List<Integer> keys : rooms) {
+            if (!keys.isEmpty()) {
                 return false;
             }
         }
@@ -16,14 +16,12 @@ class Solution {
         return true;
     }
     
-    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
-        visited[room] = true; // Mark the current room as visited
+    private void dfs(int room, List<List<Integer>> rooms) {
+        List<Integer> keys = rooms.get(room);
+        rooms.set(room, Collections.emptyList()); // Mark the current room as visited
         
-        // Visit all the neighbors
-        for (int neighbour : rooms.get(room)) {
-            if (visited[neighbour]==false) {
-                dfs(neighbour, rooms, visited);
-            }
+        for (int key : keys) {
+            dfs(key, rooms);
         }
     }
 }
