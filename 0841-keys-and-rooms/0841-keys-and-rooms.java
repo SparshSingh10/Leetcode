e90a7f6c98907c46907a2e784b3139b4abbc3e53
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-07-04 15:39:58 +0530

    Time: 2 ms (58.09%), Space: 43.9 MB (8.48%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,29 +1,33 @@
+import java.util.*;
+
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         int numRooms = rooms.size();
         boolean[] visited = new boolean[numRooms];
+        visited[0] = true; // Mark the starting room as visited
         
-        // Start the DFS from room 0
-        dfs(0, rooms, visited);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0); // Add the starting room to the queue
+        
+        while (!queue.isEmpty()) {
+            int room = queue.poll(); // Get the next room from the queue
+            
+            // Visit all the neighboring rooms
+            for (int neighbor : rooms.get(room)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true; // Mark the neighbor as visited
+                    queue.offer(neighbor); // Add the neighbor to the queue
+                }
+            }
+        }
         
         // Check if all rooms have been visited
         for (boolean roomVisited : visited) {
-            if (roomVisited==false) {
+            if (!roomVisited) {
                 return false;
             }
         }
         
         return true;
-    }
-    
-    private void dfs(int room, List<List<Integer>> rooms, boolean[] visited) {
-        visited[room] = true; // Mark the current room as visited
-        
-        // Visit all the neighbors
-        for (int neighbour : rooms.get(room)) {
-            if (visited[neighbour]==false) {
-                dfs(neighbour, rooms, visited);
-            }
-        }
     }
 }
