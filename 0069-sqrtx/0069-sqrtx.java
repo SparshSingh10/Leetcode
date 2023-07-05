88f3c1ec7fde1ac82ec56960d5efa19287f97b3d
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-07-05 15:17:42 +0530

    Time: 1 ms (100.00%), Space: 40 MB (45.14%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,27 @@
 class Solution {
     public int mySqrt(int n) {
-          return (int)Math.sqrt(n);
+        if (n == 0) {
+            return 0;
+        }
+
+        int left = 1;
+        int right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int sqrt = n / mid;
+
+            if (sqrt == mid) {
+                return mid;  // Square root found
+            }
+
+            if (sqrt < mid) {
+                right = mid - 1;  // Search in the left half
+            } else {
+                left = mid + 1;  // Search in the right half
+            }
+        }
+
+        return right;  // Return the floor value of the square root
     }
-    }
+}
