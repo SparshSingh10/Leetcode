ec8385b99fd5adbb0e9a5e837ea2882f6a510fd5
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-08-07 18:59:04 +0530

    Time: 5 ms (96.87%), Space: 49 MB (75.28%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean searchMatrix(int[][] arr, int target) {
+        int row=0;
+		int col=arr[0].length-1;
+		while(row<arr.length && col>=0) {
+			if(arr[row][col]==target) {
+				return true;
+			}
+			else if(arr[row ][col]>target) {
+				col--;
+			}
+			else {
+				row++;
+			}
+		}
+		return false;
+    }                                                                                                                                            }                                                                                                                                                                                               
