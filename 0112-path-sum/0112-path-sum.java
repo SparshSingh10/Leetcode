f92882b342ea8bfe353befeb0f9f184e154ce1de
Sparsh Singh <98759950+SparshSingh10@users.noreply.github.com>
2023-08-17 11:29:08 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (81.06%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,24 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null)
+        if (root == null)
             return false;
-        if(root.left==null && root.right ==null)
-            return (targetSum-root.val)==0;
-        return hasPathSum(root.left,targetSum-root.val)||hasPathSum(root.right,targetSum-root.val);
-    } 
+        
+        // Check if the current node is a leaf node
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
+        }
+        
+        // Recursively check the left and right subtrees
+        boolean hasLeftPathSum = hasPathSum(root.left, targetSum - root.val);
+        boolean hasRightPathSum = hasPathSum(root.right, targetSum - root.val);
+        
+        return hasLeftPathSum || hasRightPathSum;
+    }
 }
